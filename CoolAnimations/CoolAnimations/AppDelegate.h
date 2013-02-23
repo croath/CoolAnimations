0ec22c82fd45a29989e534f693c870428f62c3b6
croath <croathliu@gmail.com>
2013-02-23 23:29:23 +0800

    init proj

---
+++ CoolAnimations/CoolAnimations/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CoolAnimations
+//
+//  Created by croath on 13-2-23.
+//  Copyright (c) 2013年 croath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
